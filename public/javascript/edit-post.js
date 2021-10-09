52c29119f32d7f35c3828f4b162ec567653a30bd
Mr Whipp <dwhipp88@gmail.com>
2021-10-09 11:34:23 -0600

    refactored styling tested out some ideas

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,24 +2,35 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
+  // new START
+  // const post_url = document.querySelector('input[name="post-url"]').value;
+  const post_url = document.querySelector('input[name="post-url"]').value;
+  // new END
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
   ];
+
   const response = await fetch(`/api/posts/${id}`, {
-    method: 'PUT',
+    method: "PUT",
     body: JSON.stringify({
-      title
+      title,
+      // new START
+      post_url,
+      // new END
     }),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
 
   if (response.ok) {
-    document.location.replace('/dashboard/');
+    document.location.replace("/dashboard/");
   } else {
     alert(response.statusText);
   }
 }
 
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+document
+  .querySelector(".edit-post-form")
+  .addEventListener("submit", editFormHandler);
